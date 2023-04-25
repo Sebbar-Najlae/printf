9e5d6c63f71b60f4162688a49641a30d02f20473
Sebbar-Najlae <sebbar.najlae.dev@gmail.com>
2023-04-25 15:38:46 +0100

    handling r error

--- main.h
+++ main.h
@@ -40,8 +40,8 @@ struct fmt
 typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
-int handle_print(const char *fmt, int *i,
-va_list list, char buffer[], int flags, int width, int precision, int size);
+// int handle_print(const char *fmt, int *i,
+// va_list list, char buffer[], int flags, int width, int precision, int size);
 
 /****************** FUNCTIONS ******************/
 
@@ -52,6 +52,7 @@ int print_string(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_percent(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
+int handle_print(va_list list, char buffer[], int flags, int width, int precision, int size);
 
 /* Functions to print numbers */
 int print_int(va_list types, char buffer[],
